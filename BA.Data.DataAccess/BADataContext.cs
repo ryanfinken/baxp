e6d71d880e0c4550f7e2896df0178017b218acfc
Unknown <ryan.finken@incodeitsolutions.com>
2019-10-12 18:37:38 -0500

    Change database initializer

--- BA.Data.DataAccess/BADataContext.cs
+++ BA.Data.DataAccess/BADataContext.cs
@@ -8,12 +8,23 @@ namespace BA.Data.DataAccess {
         public virtual DbSet<Project> Projects { get; set; }
 
         public BADataContext() : base("name=BADataContext") {
-            Database.SetInitializer<BADataContext>(null);
+            Database.SetInitializer<BADataContext>(new BADataContextInitializer());
         }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder) {
             modelBuilder.Configurations.Add(new DateEntryMap());
             modelBuilder.Configurations.Add(new ProjectMap());
+        }
+    }
+
+    internal class BADataContextInitializer : IDatabaseInitializer<BADataContext> {
+        public void InitializeDatabase(BADataContext context) {
+            if (context.Database.Exists()) {
+                if (!context.Database.CompatibleWithModel(true)) {
+                    context.Database.Delete();
+                }
+            }
+            context.Database.Create();
         }
     }
 }
