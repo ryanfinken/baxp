6d7ed5894aa017883fe16df4077754ba8a05b2c0
Unknown <ryan.finken@incodeitsolutions.com>
2019-10-12 16:10:00 -0500

    Fix $onInit

--- BA.WebApp/app/dateEntries/dateEntry.controller.ts
+++ BA.WebApp/app/dateEntries/dateEntry.controller.ts
@@ -1,105 +1,108 @@
 ﻿module App.DateEntries {
-    export class DateEntryController implements angular.IController {
-        public static $inject = [
-            '$filter',
-            '$log',
-            'appConfig',
-            'dateEntriesService'
-        ];
+  export class DateEntryController implements angular.IController {
+    public static $inject = [
+      '$filter',
+      '$log',
+      'appConfig',
+      'dateEntriesService'
+    ];
 
-        public defaultFilterFormat: string;
-        public defaultMomentFormat: string;
-        public filterFormat: string;
-        public inputDate: string;
-        public logLink: string;
-        public momentDate: moment.Moment;
-        public momentFormat: string;
-        public momentUtcDate: moment.Moment;
-        public sendDate: string;
-        public serverDate: string;
+    public defaultFilterFormat: string;
+    public defaultMomentFormat: string;
+    public filterFormat: string;
+    public inputDate: string;
+    public logLink: string;
+    public momentDate: moment.Moment;
+    public momentFormat: string;
+    public momentUtcDate: moment.Moment;
+    public sendDate: string;
+    public serverDate: string;
 
-        public busyPromise: ng.IPromise<any>;
-        public loadPromise: ng.IPromise<Entities.DateEntry>;
-        public savePromise: ng.IPromise<Entities.DateEntry>;
+    public busyPromise: ng.IPromise<any>;
+    public loadPromise: ng.IPromise<Entities.DateEntry>;
+    public savePromise: ng.IPromise<Entities.DateEntry>;
 
-        private dateEntryId: number;
+    private dateEntryId: number;
 
-        constructor(
-            private $filter: ng.IFilterService,
-            private $log: ng.ILogService,
-            private appConfig: App.Services.IAppConfigService,
-            private dateEntriesService: IDateEntriesService) { }
-
-        public $onInit(): void {
-            this.$log.info('DateEntriesController > $onInit');
-
-            this.logLink = (this.appConfig.webApiUrl + 'files/logs/webapi.log').replace('/api/files', '/files');
-
-            this.defaultFilterFormat = 'short';
-            this.defaultMomentFormat = 'LLLL';
-
-            this.dateEntryId = 1;
-            this.loadDateEntry();
-        }
-
-        public applyInputDate() {
-            this.momentDate = moment(this.inputDate);
-            this.momentUtcDate = moment.utc(this.inputDate);
-            this.sendDate = this.momentDate.toISOString();
-        }
-
-        public formatFilter(momentDate: moment.Moment) {
-            if (!momentDate) {
-                return null;
-            }
-            return this.$filter('date')(momentDate.toDate(), this.filterFormat || 'short');
-        }
-
-        public formatMoment(momentDate: moment.Moment) {
-            if (!momentDate) {
-                return null;
-            }
-            return momentDate.format(this.momentFormat || 'LLLL');
-        }
-
-        public resetDateEntries() {
-            this.busyPromise = this.dateEntriesService.reset();
-
-            this.busyPromise.then(() => {
-                this.loadDateEntry();
-            });
-        }
-
-        public sendToDateToServer() {
-            let dateEntry = new Entities.DateEntry();
-            dateEntry.DateEntryId = this.dateEntryId,
-                dateEntry.FlatDateIso = this.sendDate;
-
-            this.saveDateEntry(dateEntry);
-        }
-
-        private loadDateEntry() {
-            this.loadPromise = this.dateEntriesService.getDateEntry(this.dateEntryId);
-
-            this.loadPromise.then((dateEntry) => {
-                this.inputDate = dateEntry.FlatDateIso;
-                this.applyInputDate();
-            });
-        }
-
-        private saveDateEntry(dateEntry: Entities.DateEntry) {
-            this.$log.info('DateEntriesController > saveDateEntry', dateEntry);
-
-            this.savePromise = this.dateEntriesService.saveDateEntryIso(dateEntry);
-
-            this.savePromise.then((dateEntry) => {
-                this.serverDate = dateEntry.FlatDateIso;
-                this.loadDateEntry();
-            });
-        }
+    constructor(
+      private $filter: ng.IFilterService,
+      private $log: ng.ILogService,
+      private appConfig: App.Services.IAppConfigService,
+      private dateEntriesService: IDateEntriesService
+    ) {
+      this.$onInit(); // ngRoute doesn't fire $onInit
     }
+
+    public $onInit(): void {
+      this.$log.info('DateEntriesController > $onInit');
+
+      this.logLink = (this.appConfig.webApiUrl + 'files/logs/webapi.log').replace('/api/files', '/files');
+
+      this.defaultFilterFormat = 'short';
+      this.defaultMomentFormat = 'LLLL';
+
+      this.dateEntryId = 1;
+      this.loadDateEntry();
+    }
+
+    public applyInputDate() {
+      this.momentDate = moment(this.inputDate);
+      this.momentUtcDate = moment.utc(this.inputDate);
+      this.sendDate = this.momentDate.toISOString();
+    }
+
+    public formatFilter(momentDate: moment.Moment) {
+      if (!momentDate) {
+        return null;
+      }
+      return this.$filter('date')(momentDate.toDate(), this.filterFormat || 'short');
+    }
+
+    public formatMoment(momentDate: moment.Moment) {
+      if (!momentDate) {
+        return null;
+      }
+      return momentDate.format(this.momentFormat || 'LLLL');
+    }
+
+    public resetDateEntries() {
+      this.busyPromise = this.dateEntriesService.reset();
+
+      this.busyPromise.then(() => {
+        this.loadDateEntry();
+      });
+    }
+
+    public sendToDateToServer() {
+      let dateEntry = new Entities.DateEntry();
+      dateEntry.DateEntryId = this.dateEntryId,
+        dateEntry.FlatDateIso = this.sendDate;
+
+      this.saveDateEntry(dateEntry);
+    }
+
+    private loadDateEntry() {
+      this.loadPromise = this.dateEntriesService.getDateEntry(this.dateEntryId);
+
+      this.loadPromise.then((dateEntry) => {
+        this.inputDate = dateEntry.FlatDateIso;
+        this.applyInputDate();
+      });
+    }
+
+    private saveDateEntry(dateEntry: Entities.DateEntry) {
+      this.$log.info('DateEntriesController > saveDateEntry', dateEntry);
+
+      this.savePromise = this.dateEntriesService.saveDateEntryIso(dateEntry);
+
+      this.savePromise.then((dateEntry) => {
+        this.serverDate = dateEntry.FlatDateIso;
+        this.loadDateEntry();
+      });
+    }
+  }
 }
 
 angular
-    .module('app')
-    .controller('dateEntryController', App.DateEntries.DateEntryController);
+  .module('app')
+  .controller('dateEntryController', App.DateEntries.DateEntryController);
--- BA.WebApp/app/projects/projects.controller.ts
+++ BA.WebApp/app/projects/projects.controller.ts
@@ -1,66 +1,69 @@
 ﻿module App.Projects {
-    export class ProjectsController implements angular.IController {
-        public static $inject = [
-            '$log',
-            'projectsService'
-        ];
+  export class ProjectsController implements angular.IController {
+    public static $inject = [
+      '$log',
+      'projectsService'
+    ];
 
-        public projects: Entities.Project[] = [];
+    public projects: Entities.Project[] = [];
 
-        public busyPromise: ng.IPromise<any>;
-        public loadPromise: ng.IPromise<Entities.Project[]>;
-        public savePromise: ng.IPromise<Entities.Project>;
+    public busyPromise: ng.IPromise<any>;
+    public loadPromise: ng.IPromise<Entities.Project[]>;
+    public savePromise: ng.IPromise<Entities.Project>;
 
-        constructor(
-            private $log: ng.ILogService,
-            private projectsService: IProjectsService) { }
-
-        public $onInit(): void {
-            this.$log.info('ProjectsController > $onInit');
-
-            this.loadProjects();
-        }
-
-        public addDayClick(project: Entities.Project) {
-            project.ActualCompletionDate.add(1, 'day');
-
-            this.saveProject(project);
-        }
-
-        public resetProjectDates() {
-            this.busyPromise = this.projectsService.reset();
-
-            this.busyPromise.then(() => {
-                this.loadProjects();
-            });
-        }
-
-        public subtractDayClick(project: Entities.Project) {
-            project.ActualCompletionDate.add(-1, 'day');
-
-            this.saveProject(project);
-        }
-
-        private loadProjects() {
-            this.loadPromise = this.projectsService.getProjects();
-
-            this.loadPromise.then((projects) => {
-                this.projects = projects;
-            });
-        }
-
-        private saveProject(project: Entities.Project) {
-            this.$log.info('ProjectsController > saveProject', project);
-
-            this.savePromise = this.projectsService.saveProject(project);
-
-            this.savePromise.then((p) => {
-                this.loadProjects();
-            });
-        }
+    constructor(
+      private $log: ng.ILogService,
+      private projectsService: IProjectsService
+    ) {
+      this.$onInit(); // ngRoute doesn't fire $onInit
     }
+
+    public $onInit(): void {
+      this.$log.info('ProjectsController > $onInit');
+
+      this.loadProjects();
+    }
+
+    public addDayClick(project: Entities.Project) {
+      project.ActualCompletionDate.add(1, 'day');
+
+      this.saveProject(project);
+    }
+
+    public resetProjectDates() {
+      this.busyPromise = this.projectsService.reset();
+
+      this.busyPromise.then(() => {
+        this.loadProjects();
+      });
+    }
+
+    public subtractDayClick(project: Entities.Project) {
+      project.ActualCompletionDate.add(-1, 'day');
+
+      this.saveProject(project);
+    }
+
+    private loadProjects() {
+      this.loadPromise = this.projectsService.getProjects();
+
+      this.loadPromise.then((projects) => {
+        this.projects = projects;
+      });
+    }
+
+    private saveProject(project: Entities.Project) {
+      this.$log.info('ProjectsController > saveProject', project);
+
+      this.savePromise = this.projectsService.saveProject(project);
+
+      this.savePromise.then((p) => {
+        this.loadProjects();
+      });
+    }
+  }
 }
 
 angular
-    .module('app')
-    .controller('projectsController', App.Projects.ProjectsController);
+  .module('app')
+  .controller('projectsController', App.Projects.ProjectsController);
